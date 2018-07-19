a25e59d1d8e2e77d70ed16b25d9c8707cd89d9bc
Scott Ungchusri <animoogle504@gmail.com>
2018-07-19 13:32:02 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,7 @@ class School
   end
   def add_student(student_name, grade) #has to be able to add one or more students
    # @studentArray ||= @roster[grade.to_s]
-    @studentArray = @roster[grade.to_s]
+    @studentArray ||= @roster[grade.to_s]
     @studentArray << student_name
     @roster[grade.to_s] = @studentArray
    # @studentArray = []
