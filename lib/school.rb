354cbc87748fa8381b8984c7832c264723cbde43
Scott Ungchusri <animoogle504@gmail.com>
2018-07-19 12:58:48 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,11 +8,13 @@ class School
     @school = name
     @roster = {}
   end
-  def addStudent(student_name, grade)
+  def addStudent(student_name, grade)  
     @roster[grade.to_s] = student_name
   end 
 end 
 
 oaks = School.new("Oak High")
 oaks.addStudent("billy bob joe", 9)
+oaks.addStudent("dick", 10)
+oaks.addStudent("leila", 9)
 puts oaks.roster
